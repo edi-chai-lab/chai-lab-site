5d3e5044e48ca1046871f49e7b2846f58d1fb791
Ameer Saadat <ameer.saadat99@gmail.com>
2023-10-02 00:05:44 +0100

    fix papers

--- _scripts/convert-bib-file.rb
+++ _scripts/convert-bib-file.rb
@@ -1,8 +1,10 @@
 require 'bibtex'
 require 'yaml'
+require 'fileutils'
 
 b = BibTeX.open('_data/references.bib')
 
+FileUtils.rm_rf("papers/_posts/.", secure: true)
 b.each { |x| y = x.to_hash
   y[:layout] = 'paper'
   if y[:author].length > 1
@@ -11,10 +13,11 @@ b.each { |x| y = x.to_hash
     ref = x.author[0][1].to_s +", " + x.year.to_s
   end
   y[:ref] = ref
-  f_name = "papers/_posts/01-01-"+x.year.to_s[-2..-1]+"-"+x.title.to_s.split[0]+".md".tr('\"{}', '')
+  f_name = "papers/_posts/01-01-"+x.year.to_s[-2..-1]+"-"+x.title.to_s.tr('\"{}', '').tr(' ', '-')+".md"
   puts f_name
+  y.transform_values { |value| '"' + value.to_s + '"'}
   y = y.transform_keys(&:to_s).to_yaml
-  y = y.tr('\"{}', '')
+  y = y.tr('{}', '')
   y = y + '---'
   File.write(f_name, y)
  }
