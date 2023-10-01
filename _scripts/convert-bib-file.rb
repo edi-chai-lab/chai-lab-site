87e5a325f9a764e46e035fbe50fb47fa36e3d93a
Ameer Saadat <ameer.saadat99@gmail.com>
2023-10-02 00:53:07 +0100

    fix external urls

--- _scripts/convert-bib-file.rb
+++ _scripts/convert-bib-file.rb
@@ -1,10 +1,10 @@
 require 'bibtex'
 require 'yaml'
-require 'fileutils'
 
 b = BibTeX.open('_data/references.bib')
+Dir.glob("papers/_posts/*.md").each { |file| File.delete(file)
+puts file}
 
-FileUtils.rm_rf("papers/_posts/*.md", secure: true)
 b.each { |x| y = x.to_hash
   y[:layout] = 'paper'
   if y[:author].length > 1
@@ -13,15 +13,16 @@ b.each { |x| y = x.to_hash
     ref = x.author[0][1].to_s +", " + x.year.to_s
   end
   y[:ref] = ref
-  y[:external_url] = y.delete :url
 
   y.transform_values { |value| '"' + value.to_s + '"'}
-  y = y.transform_keys(&:to_s).to_yaml
-  y = y.tr('{}', '')
+  y = y.transform_keys(&:to_s)
+  y["external_url"] = y.delete "url" if y.key?("url")
+  y = y.to_yaml.tr('{}', '')
   y = y + '---'
 
   f_name = "papers/_posts/01-01-"+x.year.to_s[-2..-1]+"-"+x.title.to_s.tr('\"{}', '').tr(' ', '-')+".md"
   puts f_name
 
+  puts y
   File.write(f_name, y)
  }
